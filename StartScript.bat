5865a0f4ba6b6f1fd8f6db62e073a2e48678d3f8
ExAzE <131683195+ExAzZe@users.noreply.github.com>
2026-06-02 13:59:38 +0200

    Update StartScript.bat

--- StartScript.bat
+++ StartScript.bat
@@ -1,6 +1,3 @@
 @echo off
-:: Lance ScriptVM.ps1 en contournant l'ExecutionPolicy
-:: Les deux fichiers doivent être dans le même dossier
-
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ScriptVM.ps1"
 pause
