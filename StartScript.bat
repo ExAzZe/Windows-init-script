14bca6565430d58476ea671e0e1c6a10eb825649
ExAzE <131683195+ExAzZe@users.noreply.github.com>
2026-06-03 15:21:47 +0200

    Implement admin elevation for StartScript.bat
    Add elevation check and restart script as admin if needed.

--- StartScript.bat
+++ StartScript.bat
@@ -1,3 +1,11 @@
 @echo off
+net session >nul 2>&1
+if %errorlevel% == 0 goto :admin
+ 
+echo Elevation requise - relancement en administrateur...
+powershell -Command "Start-Process '%~f0' -Verb RunAs"
+exit /b
+ 
+:admin
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0ScriptVM.ps1"
 pause
